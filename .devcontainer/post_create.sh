fa3f2fb466cf5af639e52ad56e09f0fff489919a
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2023-09-27 20:20:59 +0000

    Add .devcontainer files

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,26 @@
+# For writing commands that will be executed after the container is created
+
+# Installs `project_name` as local library without resolving dependencies (--no-deps)
+# python3 -m pip install -e ./src/project_name --no-deps
+
+# # Install datasets from S3
+# # This script will download the datasets from S3 and place them in the correct directory structure
+# # This script should be run from the root of the repository
+
+# # Define the base directory path
+# base_dir="${HOME}/data/ephys"
+
+# # Create the base directory
+# mkdir -p "$base_dir"
+
+# # Define an array of dataset names
+# dataset_names=("path/dataset_name_001" "path/dataset_name_002" "path/dataset_name_003")
+
+# # Loop through the dataset names
+# for sub_dir in "${dataset_names[@]}"; do
+#   # Create directories
+#   mkdir -p "$base_dir/$sub_dir"
+
+#   # Download data from S3
+#   aws --endpoint https://s3-west.nrp-nautilus.io s3 cp "s3://braingeneers/ephys/$sub_dir" "$base_dir/$sub_dir" --recursive --no-sign-request
+# done
